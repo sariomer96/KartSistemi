9f5577861d5f4b6b693a840a9f93a7e02485b6d0
sariomer96 <omersari_96@hotmail.com>
2019-01-07 19:55:09 +0300

    kart sistemi eklendi.

---
+++ sonDeneme/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace sonDeneme
+{
+    class Program
+    {
+        
+        static SerialPort port;
+        static System.Threading.Timer TTimer;
+
+        static void Main(string[] args)
+        {
+
+            //port = new SerialPort("COM4", 9600);
+
+
+            port = new SerialPort("COM3");
+            port.BaudRate = 9600;
+            port.Parity = Parity.None;
+            port.StopBits = StopBits.One;
+            port.DataBits = 8;
+            port.Handshake = Handshake.None;
+            port.RtsEnable = true;
+
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            TTimer = new System.Threading.Timer(new TimerCallback(TickTimer), null, 1000, 1500);
+            try
+            {
+                port.Open();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void TickTimer(object state)
+        {
+
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            Thread.Sleep(1500);
+        }
+
+        public static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            try
+            {
+                string konua = port.ReadLine();
+                if (string.IsNullOrEmpty(konua) == false)
+                {
+                    YurtOtomasyonuEntities1 db = new YurtOtomasyonuEntities1();
+                    GirisCikis grs = new GirisCikis();
+                    Ogrenciler ogr = new Ogrenciler();
+                    int id = 1;
+                    foreach (var i in db.Ogrenciler)
+                    {
+                        if (konua == i.KartID)
+                        {
+                            id = i.ID;
+                            
+                            //  grs.Ogrenciler_ID 
+                           
+
+                        }
+                    }
+
+                    grs.YurtGiris = DateTime.Now;
+                    grs.Ogrenciler_ID = id;
+                    db.GirisCikis.Add(grs);
+                    Console.WriteLine(grs.YurtGiris);
+
+                    db.SaveChanges();
+
+
+                }
+                else
+                {
+                    //Console.Write(port.ReadExisting());
+                }
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+       
+    }
+}
---
+++ sonDeneme/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("sonDeneme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sonDeneme")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ae6b66ee-34f8-4b58-b841-8c6c5ebdf9de")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
